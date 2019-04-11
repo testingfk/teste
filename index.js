b53ef26fa8f7c797b6d2475c29cc96915c90a648
Jorge Junior - Unidade de Sistemas TI <M88170@UNIMEDRJ.ROOT>
2019-04-11 09:24:47 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
 const axios = require('axios');
 const coins = require('./lib/constants'); 
+const express = require('express');
+const rates = require('./lib/rates');
+const app = express()
+const port = 3000
+ 
 
-getExchange('USD','BRL',0);
+console.log(rates);
+
 function getExchange(coinFrom, coinTo, amount){
+    let result;
     if(isNaN(amount)) throw new Error("Please insert a valid amount.");
 
-    if(!coins.AVALIABLE_COINS.arr_coins.includes(coinFrom)) throw new Error("Not a valid .");
+    if(!coins.AVALIABLE_COINS.arr_coins.includes(coinFrom)) throw new Error("Not a valid coin.");
 
-
-    if( coins.AVALIABLE_COINS.arr_coins.includes(coinFrom) && 
-    coins.AVALIABLE_COINS.arr_coins.includes(coinTo)){
-        axios.get("https://api.exchangeratesapi.io/latest?base" + coinFrom).then(resp => {
-            console.log(resp.data.rates[coinTo]);
-        });
-    }
+    if(!coins.AVALIABLE_COINS.arr_coins.includes(coinTo)) throw new Error("Not a valid coin.");
+    
+    axios.get("https://api.exchangeratesapi.io/latest?base" + coinFrom).then(resp => {
+        //console.log(resp.data.rates[coinTo]);
+        result = (amount * resp.data.rates[coinTo]).toFixed(2);            
+        });   
+    return result;
 }
+
